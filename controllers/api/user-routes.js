483db5ef9c74b43061681136434c19e212c9ae9c
Preston Fassbender <fassbenderp0551@gmail.com>
2022-03-04 16:05:06 -0600

    user model with bcrypt hooks and password compare, user routes outline

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// get all users
+// get one user
+// create a user
+// login user session
+// logout user session
+// update user
+// delete user
+
+module.exports = router;
