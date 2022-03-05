1374ed2effcd8292e970e3b72dc9b284808436f9
Preston Fassbender <fassbenderp0551@gmail.com>
2022-03-05 12:01:52 -0600

    comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -19,8 +19,8 @@ router.post('/', withAuth, (req, res) => {
         Comment.create({
             comment_text: req.body.comment_text,
             post_id: req.body.post_id,
-            user_id: req.body.user_id
-            //user_id: req.session.user_id
+            //user_id: req.body.user_id
+            user_id: req.session.user_id
         })
             .then(dbCommentData => res.json(dbCommentData))
             .catch(err => {
